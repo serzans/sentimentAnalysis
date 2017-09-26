508cd4ab5cc6f8fdfe7ad6585dcdff7c2cc0de25
Zeekster4 <hazem.hassan70@yahoo.com>
2017-09-26 10:51:11 +0100

    Added place holder reddit API calls
    got the API calls put in for the comments in the top posts in /r/politics

--- getRedditComments.py
+++ getRedditComments.py
@@ -1,5 +1,26 @@
 import pandas as pd
 import numpy as np
+import praw
+import datetime 
+
+#change to timestamp
+
+def get_date(comment):
+    time = comment.created
+    return datetime.datetime.fromtimestamp(time)
+
+# Create a Reddit instance
+reddit = praw.Reddit(user_agent = "sentimentAnalysis", client_id = "zwiURjj8Ml4KmA",
+		     client_secret = "I0bb_hg7CjgK6za3CKEznqBP8XQ")
+#list of submissions and comments we're interested in
+submissions = reddit.subreddit("politics").top(limit = None)
+comments = []
+iter = 0
+while (len(comments)<30000)):
+	submission = submissions[iter]
+	submission.comments.replace_more(limit = 0)
+	comments.extend(submission.comments.list())
+	
 
 filename = 'redditComments.h5'
 
@@ -17,12 +38,12 @@ hdf.put('d1',df,format='table',data_columns=True)
 df_temp = pd.DataFrame(index=range(10000),columns=columns)
 for x in range(1,30000):
 	
-	# Need API calls here
+	comment = comments[x]
 
-	df_temp.iloc[x%10000]['comment'] = 'test'
-	df_temp.iloc[x%10000]['timestamp'] = 'test'
-	df_temp.iloc[x%10000]['points'] = 'test'
-	df_temp.iloc[x%10000]['user'] = 'test'
+	df_temp.iloc[x%10000]['comment'] = comment.body
+	df_temp.iloc[x%10000]['timestamp'] = get_date(comment)
+	df_temp.iloc[x%10000]['points'] = comment.score
+	df_temp.iloc[x%10000]['user'] = comment.author
 	df_temp.iloc[x%10000]['category'] = 'test'
 
 	if x%10000==0:
